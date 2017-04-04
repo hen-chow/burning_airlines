42d57d7dbefeee25acadc8939ca368e4dc702695
Kevin Li <hoi.li@unswalumni.com>
2017-04-04 14:51:40 +1000

    Flight Controller added

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -6,7 +6,7 @@ class FlightsController < ApplicationController
 
   def create
     flight = Flight.create(number: params[:number], date: params[:date], arrival: params[:arrival], departure: params[:departure], plane_id: params[:plane_id])
-    
+
     render json: flight
   end
 
