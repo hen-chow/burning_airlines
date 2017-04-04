7944b11996b5320e7a94fb198d03292e8634f825
Kevin Li <hoi.li@unswalumni.com>
2017-04-04 14:30:17 +1000

    Rails routes and controllers established

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "flights#app"
+  resources :flights, only: [:create, :index]
+  resources :airplanes, only: [:create, :index]
+  resources :reservations, only: [:create, :index]
+  resources :users, only: [:create, :index]
 end
