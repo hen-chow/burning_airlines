840234b66782efe2ce3d043996013e2fcd88c5d3
Kevin Li <hoi.li@unswalumni.com>
2017-04-05 09:05:57 +1000

    Rendered seat plan on airplane page

--- app/assets/javascripts/views/AirplaneView.js
+++ app/assets/javascripts/views/AirplaneView.js
@@ -4,18 +4,54 @@ App.AirplaneView = Backbone.View.extend({
   el: "#main",
 
   events: {
-    "click": "show"
+    "click #new_plane": "show",
+    "click #save_plane": "save",
+    "click #cancel": "cancel"
   },
 
   render: function () {
     this.$el.html($("#createPlaneTemplate").html());
     // var view = new App.AirplanesListView({collection: this.collection});
     // view.render();
+    // this.$el.html($("#seatTemplate").html());
 
   },
 
   show: function(){
     $("#plane_form").css("display", "inline");
+  },
+
+  save: function(){
+    $("#seats").html("");
+    var name = $("#plane_name").val();
+    var rows = $("#plane_row").val();
+    var cols = $("#plane_col").val();
+    var seats = $("#seats");
+    var table = $("<table>");
+
+    seats.append(table);
+    table.append("<caption>" + name + " - " + "rows: " + rows + ", cols: " + cols + "</caption>");
+
+    for(var r = 0; r < rows; r++) {
+      var tr = $("<tr>");
+      $("<th>" + (r+1) + "</th>").appendTo(tr);
+      for (var c = 0; c < cols; c++) {
+        // $("<th>" + (c+1) + "</th>").appendTo(table);
+        $("<td></td>").appendTo(tr);
+        }
+      tr.appendTo(table);
+    }
+
+    $("caption").css("text-align", "left");
+    $("table").css("margin", "100px auto");
+    $("td").css({
+      border: "solid 1px black",
+      padding: "10px"
+    });
+  },
+
+  cancel: function(){
+    $("#plane_form").css("display", "none");
   }
 
 });
