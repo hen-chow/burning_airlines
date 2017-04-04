21c060ee893974c5faaea89cc0daf8bced5f8193
Kevin Li <hoi.li@unswalumni.com>
2017-04-04 14:24:08 +1000

    Rails models created

---
+++ db/migrate/20170404041139_create_airplanes.rb
@@ -0,0 +1,11 @@
+class CreateAirplanes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :airplanes do |t|
+      t.string :name
+      t.integer :row
+      t.integer :column
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20170404041405_create_flights.rb
@@ -0,0 +1,13 @@
+class CreateFlights < ActiveRecord::Migration[5.0]
+  def change
+    create_table :flights do |t|
+      t.integer :number
+      t.date :date
+      t.string :to
+      t.string :from
+      t.integer :plane_id
+
+      t.timestamps
+    end
+  end
+end
