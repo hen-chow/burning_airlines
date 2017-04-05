9f52fd33e0cac5b66277a23fa6d409b1e5d77c42
Henrietta Chow <henchow@gmail.com>
2017-04-05 16:03:45 +1000

    completed flight create page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -6,7 +6,7 @@ class AdminController < ApplicationController
   end
 
   def create_airplanes
-    @airplane = Airplane.new(clean_params)
+    @airplane = Airplane.new(clean_params_airplane)
     if @airplane.save
       redirect_to index_airplanes_path
       flash[:success] = "An airplane is created."
@@ -16,18 +16,30 @@ class AdminController < ApplicationController
     end
   end
 
-  def clean_params
+  def index_flights
+    @flights = Flight.all
+    @airplanes = Airplane.all
+  end
+
+  def create_flights
+    @flight = Flight.new(clean_params_flight)
+    if @flight.save
+      redirect_to index_flights_path
+      flash[:success] = "A flight is created."
+    else
+      redirect_to index_flights_path
+      flash[:error] = "Fatal error."
+    end
+  end
+
+  private
+
+  def clean_params_airplane
     params.permit(:name, :row, :column)
   end
 
-  # def index_flights
-  #   render json: Flight.all
-  # end
-  #
-  # def create_flights
-  #   flight = Flight.create(number: params[:number], date: params[:date], arrival: params[:arrival], departure: params[:departure], airplane_id: params[:airplane_id])
-  #
-  #   render json: flight
-  # end
+  def clean_params_flight
+    params.permit(:number, :date, :arrival, :departure, :airplane_id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get "/admin/airplanes", to: "admin#index_airplanes", as: "index_airplanes"
   post "/admin/airplanes", to: "admin#create_airplanes", as: "create_airplanes"
-  get "/admin/flights", to: "admin#index_flights"
-  post "/admin/flights", to: "admin#create_flights"
+  get "/admin/flights", to: "admin#index_flights", as: "index_flights"
+  post "/admin/flights", to: "admin#create_flights", as: "create_flights"
 
   get "login", to: "sessions#new", as: "login"
   post "login", to: "sessions#create"
